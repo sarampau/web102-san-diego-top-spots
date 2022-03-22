7457eebf8216388ca17ef11460243f65c565654e
Manny <mannysth@yahoo.com>
2022-03-22 13:27:05 -0700

    finishes project and passes tests

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-// write your code here
-
+    $.getJSON('data.json', data => {
+        for (let i = 0; i < data.length; i++) {
+            let name  = data[i].name;
+            let des = data[i].description;
+            let location = data[i].location;
+            $('#table').append('<tr>' +
+                '<td>' + name + '</td>' +
+                '<td>' + des + '</td>' +
+                '<td>' + location[0] + ', ' + location[1] + '</td></tr>' );
+        }
+    });
+    
 });
